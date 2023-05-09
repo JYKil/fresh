a174e690f1b9102cd9c3727e6d95b528d9b9377a
JYKil <kilga2402@gmail.com>
2023-05-10 07:51:03 +0900

    Apply product array map

--- app/list/page.js
+++ app/list/page.js
@@ -1,15 +1,18 @@
 import Image from 'next/image'
 
 export default function List() {
+  let productList = ['Tomatoes', 'Pasta', 'Coconut']
   return (
     <div>
       <h2 className='title-sub'>상품목록</h2>
-      <div className="food">
-        <h4>상품1 $40</h4>
-      </div>
-      <div className="food">
-        <h4>상품2 $40</h4>
-      </div>
+      {
+        productList.map((a) => {
+          return (
+            <div className="food">
+              <h4>{a} $40</h4>
+            </div>)
+        })
+      }
     </div>
   )
 }
